b319be7f4a7dd5d42b72a9777eb1f74bd1a35d84
jay <enlqn1010@gmail.com>
2020-05-03 17:52:15 +0900

    third version

--- transaction.py
+++ transaction.py
@@ -10,7 +10,7 @@ class account:
         print("이름:")
         self.name = input()
         print("예금:")
-        self.money = input()
+        self.money = int(input())
         total_money = self.money
         print("##계좌개설을 완료하였습니다##")
 
@@ -20,6 +20,24 @@ def lookup():
         print("계좌번호:",person.accountnum,"/","이름:",person.name,"/","잔액",person.money,"원","/")
     print("===================")
 
+def deposit():
+    print("======입금하기======")
+    deposit_account = input("입금하실 계좌번호를 입력하세요:")
+    for person in persons:
+        if person.accountnum == deposit_account:
+            print("계좌이름:",person.name)
+            print("계좌잔고:",person.money,"원")
+            deposit_money = int(input("입금하실 금액을 입력해주세요:"))
+            person.money += deposit_money
+            print("##계좌잔고:",person.money,"원##")
+            print("##입금이 완료되었습니다.##")
+            break
+
+def withdraw():
+    ##우석님 출금하는 함수 입니다.! :)
+    ##deposit하는 함수와 비슷하니 참고하세요~
+    return 0
+    
 while(True):
     print("======Bank Menu======")
     print("1. 계좌개설")
@@ -33,11 +51,15 @@ while(True):
     if a == '1':
         newaccount = account()
         persons.append(newaccount)
+    elif a== '2':
+        deposit()
+    ##elif a == '3':
+    
     elif a=='4':
         lookup()
     elif a == '5':
+        print("##은행프로그램을 종료합니다.##")
         sys.exit()
     else:
         print("제공되지 않는 기능입니다. 다시 골라주세요")
 
-print("Hello world")
