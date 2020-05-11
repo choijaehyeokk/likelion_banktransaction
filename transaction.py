5e89078e83df0c57c24d0bd6478337ee9054442f
jay <enlqn1010@gmail.com>
2020-05-11 11:58:50 +0900

    eight version

--- transaction.py
+++ transaction.py
@@ -23,6 +23,10 @@ def lookup():
 def deposit():
     print("\n======입금하기======")
     deposit_account = input("입금하실 계좌번호를 입력하세요:")
+
+    if not persons:
+        print("개설된 계좌가 없습니다.\n")
+        return
     for person in persons:
         if person.accountnum == deposit_account:
             print("계좌이름:",person.name)
@@ -33,14 +37,14 @@ def deposit():
             print("##입금이 완료되었습니다.##")
             print("=====================\n")
             break
-    if deposit_account != person.accountnum:
-        print("입금하실 계좌가 없습니다.")
-        print("=====================\n")
+        
 
 def withdraw():
     print("\n======출금하기======")
 
     withdraw_account = input("출금하실 계좌번호를 입력하세요:")
+    if not persons:
+        print("개설된 계좌가 없습니다.\n")
     for person in persons:
         if person.accountnum == withdraw_account:
             print("계좌이름:",person.name)
@@ -55,11 +59,32 @@ def withdraw():
             print("##계좌잔고:",person.money,"원##")
             print("##출금이 완료되었습니다.##")
             break
-    if withdraw_account != person.accountnum:
-        print("출금하실 계좌가 없습니다.")
-        print("=====================\n")
         
-
+def deposit_interest():
+    current = ''
+    print("======예금이자 계산하기======")
+    interets_account =input("계좌를 입력해주세요: ")
+    if not persons:
+        return 0
+    else:
+        for person in persons:
+            if person.accountnum == interets_account:
+                current = person
+            
+        if not current:
+            return 1
+               
+        year = int(input("몇 년으로 하시겠습니까 : "))
+        interest_rate = float(input("금리가 몇 %입니까 :"))
+        a= input("단리 / 복리 중에 선택해주세요:")
+        if a=='단리':
+            fv = current.money * (1+((0.01*interest_rate) * year))
+            return fv
+        elif a == '복리':
+            fv = current.money * ((1+(0.01*interest_rate))**year)
+            return round(fv,2)
+        else:
+            return 2
 while(True):
     print("======Bank Menu======")
     print("1. 계좌개설")
@@ -67,6 +92,7 @@ while(True):
     print("3. 출금하기")
     print("4. 전체조회")
     print("5. 프로그램 종료")
+    print("6. 예금 연이자 계산하기")
     print("=====================\n")
 
     a = input("어떤 업무를 수행하시겠습니까?")
@@ -82,6 +108,16 @@ while(True):
     elif a == '5':
         print("\n##은행프로그램을 종료합니다.##\n")
         break
+    elif a== '6':
+        result = deposit_interest()
+        if result == 0:
+            print("등록된 계좌가 없습니다")
+        elif result == 1:
+            print("해당 계좌가 없습니다.")
+        elif result == 2:
+            print("잘못된 입력입니다.")
+        else:
+            print("세전 %.2f 원입니다." %result)
     else:
         print("제공되지 않는 기능입니다. 다시 골라주세요")
 
